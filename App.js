c0f247be2e41c97afe4a0b4c2e607eea84abfa03
Beam-ADVWS <napapol@advws.com>
2020-01-27 12:05:24 +0700

    AllCard by Beam

---
+++ App.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text
+} from "react-native";
+import Login from './screens/login'
+import Register from './screens/Register'
+import LoginNumber from './screens/loginNumber'
+import OTPscreen from './screens/OTPsreen'
+
+
+export default class App extends Component{
+  changePage = (page) =>{
+    this.setState({
+      page:page,
+    })
+    console.log(this.state.page);
+  }
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      page:"Login",
+    }
+  }
+  render(){
+    var DisplayPage = <Login changePage ={this.changePage}/>
+    const{ 
+      email="mydeverlop@advws.com",
+      nameUser="มะลอง",
+      lastNameUser="ก๊อกแก๊ง"
+    } = this.props
+    switch(this.state.page) {
+        case 'Login':
+            DisplayPage = <Login changePage ={this.changePage}/>
+          break;
+        case 'LoginFacebook':
+            DisplayPage = <Register changePage ={this.changePage} email ={email}  nameUser={nameUser} lastNameUser={lastNameUser}/>
+          break;
+        case 'LoginGoogle':
+            DisplayPage = <Register changePage ={this.changePage} email ={email}/>
+          break;
+        case 'LoginNumber':
+            DisplayPage = <LoginNumber changePage ={this.changePage}/>
+          break;
+        case 'Register':
+            DisplayPage = <Register changePage ={this.changePage}/>
+          break;
+        case 'OTPscreen':
+            DisplayPage = <OTPscreen changePage={this.changePage}/>
+          break;
+    }
+    return(
+      <View >
+        {DisplayPage}
+      </View>
+    )
+  }
+}
---
+++ screens/login.js
@@ -0,0 +1,53 @@
+import React,{Component} from 'react';
+import { Text, View, Image, TouchableOpacity} from 'react-native';
+import StyleSheet from '../src/components/componentsLogin/styles'
+import Icon from 'react-native-vector-icons/FontAwesome';
+var thisPage = ""
+export default class Login extends React.Component {
+    Goto = () => {
+        var page = thisPage
+        console.log("this Page : "+thisPage)
+        const {changePage}=this.props;
+        this.changePage= changePage;
+        this.changePage(page);
+    }
+    render() {
+        return (
+            <View style={StyleSheet.container}>    
+                <Image
+                    source={require('../assets/image/logo.png')}
+                    style={{ width: 180, height: 180 }}
+                />
+                <Text style={StyleSheet.textLogin1}>เข้าสู่ระบบสมาชิก</Text>
+                <TouchableOpacity style={StyleSheet.buttonLoginNumber}  onPress={()=>this.Goto(thisPage="LoginNumber")}>
+                    <Text style={StyleSheet.textbuttonNumber}>
+                        เข้าสู่ระบบด้วยเบอร์โทรศัพท์
+                </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={StyleSheet.buttonLoginFacebook} onPress={()=>this.Goto(thisPage="LoginFacebook")}>
+                    <Text style={StyleSheet.textbuttonFacebookAndGoogle}>
+                        เข้าสู่ระบบด้วย  <Icon name="facebook" color="#FFFFFF" size={20} />
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={StyleSheet.buttonLoginGoogle} onPress={()=>this.Goto(thisPage="LoginGoogle")}>
+                    <Text style={StyleSheet.textbuttonFacebookAndGoogle}>
+                        เข้าสู่ระบบด้วย  <Icon name="google-plus" color="#FFFFFF" size={20} />
+                    </Text>
+                </TouchableOpacity>
+                <View style={{ justifyContent: 'space-between', marginTop: 100, flexDirection: 'row' }}>
+                    <TouchableOpacity >
+                        <Text style={StyleSheet.textLogin2}> นโยบายความเป็นส่วนตัว </Text>
+                    </TouchableOpacity>
+                    <Text style={{ fontSize: 10, color: '#FFFFFF' }}>|</Text>
+                    <TouchableOpacity>
+                        <Text style={StyleSheet.textLogin2}> เงื่อนไขการใช้ </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+}
+
+
+
+
